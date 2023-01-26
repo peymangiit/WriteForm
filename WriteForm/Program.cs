3e80cfaa655774886b01b23f7cf5713205ff61d5
Ali Aghajani <Aghajani.dev@gmail.com>
2023-01-26 14:38:57 +0330

    input method readline() for string/integer varibles

--- WriteForm/Program.cs
+++ WriteForm/Program.cs
@@ -11,14 +11,25 @@ namespace WriteForm
         static void Main(string[] args)
         {
             string name, family;
+            int birthday,id_number;
             Console.WriteLine("please write your name:");
             name = Console.ReadLine();
             Console.WriteLine("please write your family:");
             family = Console.ReadLine();
+            Console.WriteLine("please write your birthday:(year only)");
+            birthday = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please write your id-Number:(5 digit)");
+            id_number = Convert.ToInt32(Console.ReadLine());
             //name = "hamid";
            // family = "Roshan";
             Console.WriteLine("                          ┌──────────────────────────────────────────────────────────┐");
             Console.WriteLine("                          |   Name |"+name+"    |                   Family| "+family+" |    |");
+            Console.WriteLine("                          |                                                          |");
+            Console.WriteLine("                          |                                                          |");
+            Console.WriteLine("                          | birthday |{0}|                      id-Number |{1}|   |",birthday,id_number);
+            Console.WriteLine("                          |                                                          |");
+            Console.WriteLine("                          |                                                          |");
+            Console.WriteLine("                          |                                                          |");
         }
     }
 }
