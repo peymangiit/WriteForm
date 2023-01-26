4d0788d2cabc185c2f10eac70aac4125214258d5
Ali Aghajani <Aghajani.dev@gmail.com>
2023-01-26 13:44:34 +0330

    form start test

---
+++ WriteForm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string name = "hamid";
+            Console.WriteLine("                          ┌──────────────────────────────────────────────────────────┐");
+            Console.WriteLine("                          |   Name |{0}    |                   Family| family |      |",name);
+        }
+    }
+}
