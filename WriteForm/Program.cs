33b1ebdfc8343edc9abe06c64d3b7d8051cf831c
Ali Aghajani <Aghajani.dev@gmail.com>
2023-01-27 11:49:36 +0330

    build form structure

--- WriteForm/Program.cs
+++ WriteForm/Program.cs
@@ -10,26 +10,59 @@ namespace WriteForm
     {
         static void Main(string[] args)
         {
-            string name, family;
-            int birthday,id_number;
-            Console.WriteLine("please write your name:");
-            name = Console.ReadLine();
-            Console.WriteLine("please write your family:");
-            family = Console.ReadLine();
-            Console.WriteLine("please write your birthday:(year only)");
-            birthday = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("please write your id-Number:(5 digit)");
-            id_number = Convert.ToInt32(Console.ReadLine());
-            //name = "hamid";
-           // family = "Roshan";
+            //مشخص کردن نوع متغییر
+           string name, family;
+         //   int birthday,id_number;
+            //گرفتن نام از کاربر
+           // Console.WriteLine("please write your name:");
+            //name = Console.ReadLine();
+            //Console.WriteLine("please write your family:");
+            //family = Console.ReadLine();
+            //برای اعداد تغییر متغییر به رشته
+            //Console.WriteLine("please write your birthday:(year only)");
+            //birthday = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("please write your id-Number:(5 digit)");
+            //id_number = Convert.ToInt32(Console.ReadLine());
+            name = "hamid";
+            family = "Roshan";
+
+
             Console.WriteLine("                          ┌──────────────────────────────────────────────────────────┐");
-            Console.WriteLine("                          |   Name |"+name+"    |                   Family| "+family+" |    |");
+            Console.WriteLine("                          |        ┌──────────┐                    ┌────────────┐    | ");
+            Console.WriteLine("                          |   Name |  name    |              Family|   family   |    |");
+            Console.WriteLine("                          |        └──────────┘                    └────────────┘    |");
             Console.WriteLine("                          |                                                          |");
+            Console.WriteLine("                          |           ┌────────┐                  ┌────────────┐     |");
+            Console.WriteLine("                          |  birthday |        |        ID-Number |            |     |");
+            Console.WriteLine("                          |           └────────┘                  └────────────┘     |");
             Console.WriteLine("                          |                                                          |");
-            Console.WriteLine("                          | birthday |{0}|                      id-Number |{1}|   |",birthday,id_number);
+            Console.WriteLine("                          |           ┌──────────────────────────────────────────┐   |");
+            Console.WriteLine("                          |   Address |                                          |   |");
+            Console.WriteLine("                          |           └──────────────────────────────────────────┘   |");
             Console.WriteLine("                          |                                                          |");
+            Console.WriteLine("                          |                    ┌────────────────────┐                |");
+            Console.WriteLine("                          |    Grade's Average |                    |                |");
+            Console.WriteLine("                          |                    └────────────────────┘                |");
             Console.WriteLine("                          |                                                          |");
-            Console.WriteLine("                          |                                                          |");
+            Console.WriteLine("                          └──────────────────────────────────────────────────────────┘");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Console.WriteLine("                          | birthday |{0}|                      id-Number |{1}|   |",birthday,id_number);
         }
     }
 }
